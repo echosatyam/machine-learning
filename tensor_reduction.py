4799ee825f507b3509358fafc7f743e939a9f206
satyamsingh19981601@gmail.com <satyamsingh19981601@gmail.com>
2019-07-15 15:41:22 +0530

    Tensor Reduction and argmax

--- tensor_reduction.py
+++ tensor_reduction.py
@@ -19,3 +19,16 @@ print(t1.sum(dim=1))
 print(t1[0].sum())
 print(t1[1].sum())
 print(t1[0].sum())
+
+
+print('\n\nargmax and stuffs below\n\n')
+t = torch.tensor([
+    [1,0,0,2],
+    [0,3,3,0],
+    [4,0,0,5]
+],dtype=torch.float32)
+print(t.max())
+print(t.argmax())
+print(t.flatten())
+print(t.max(dim=0))
+print(t.argmax(dim=0))
